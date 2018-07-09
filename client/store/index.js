f93166d61bbf5ad9ac1b5df4e861b054b80d3101
donatello <agvarea@gmail.com>
2018-07-08 21:15:13 -0300

    Fix context to use nuxt generate

--- client/store/index.js
+++ client/store/index.js
@@ -4,7 +4,7 @@ export const actions = {
     nuxtServerInit({ commit }, { req }) {
         let user = null
         let card = []
-        if (req.headers.cookie) {
+        if (req && req.headers.cookie) {
             const parsed = cookieparser.parse(req.headers.cookie)
             
             user = (parsed.user && JSON.parse(parsed.user)) || null
